5f457f4a2c2c81464cad8fa5d52e9fb5d0742fdb
kevin-ko <kevin.alex.ko@gmail.com>
2023-07-21 15:01:20 +0800

    hot reload in stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('my first app'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Center(
-        child: Text ('Hello World',
+    home: Home()
+  ));
+}
+
+class Home extends StatelessWidget {
+  //const ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  //return const Placeholder();
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text ('Hello World',
         style: TextStyle(
           fontSize: 20.0,
           fontWeight: FontWeight.bold,
           letterSpacing: 2.0,
           color: Colors.grey[600],
           fontFamily: 'IndieFlower',
-        ),
-        ),
       ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {  },
-          child:Text('click'),
-          backgroundColor: Colors.red[600],
-        ),
     ),
-  ));
+  ),
+      floatingActionButton: FloatingActionButton(
+      onPressed: () { },
+      child:Text('click me'),
+      backgroundColor: Colors.red[600],
+      ),
+    );
+  }
 }
-
