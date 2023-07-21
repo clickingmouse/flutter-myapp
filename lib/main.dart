3767c1e67f3f632ae24fea4f629b6f9549027f07
kevin-ko <kevin.alex.ko@gmail.com>
2023-07-21 14:43:29 +0800

    new font

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,20 @@ void main() {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text ('Hello World'),
-
+        child: Text ('Hello World',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+        ),
       ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {  },
           child:Text('click'),
+          backgroundColor: Colors.red[600],
         ),
     ),
   ));
